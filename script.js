7c9c23798d29c1d0faa24020fd0778485d9b0a40
Gabbyajias <gabbsajias@gmail.com>
2024-05-31 13:29:26 -0400

    all doneeeeee

--- script.js
+++ script.js
@@ -3,93 +3,113 @@
 const teamMembers = [
   {
     name: "Jack Hughes",
-    number: "86",
+    number: 86,
     position: "forward",
-    age: "22",
+    age: 22,
     skills: ["high speed', 'Stickhandling', 'Shooting', 'Skating"],
     biography:
       "Jack Hughes is a highly skilled hockey player known for his exceptional speed, agility, and playmaking ability. As the first overall pick in the 2019 NHL Draft, he quickly made an impact in the league with his impressive hockey IQ and versatility on the ice. With a promising career ahead, Hughes continues to be a key player for his team, showcasing his talent and determination every game.",
+      imageURL:"jack.webp"
+
   },
   {
     name: "Erik Haula",
-    number: "56",
+    number: 56,
     position: "forward",
-    age: "33", 
+    age: 33, 
     skills: ["versatility on the ice', 'agility', 'Shooting', 'Skating"],
     biography:
       "Erik Haula, a Finnish professional ice hockey forward, has showcased his talents in the NHL, demonstrating remarkable speed, agility, and a keen scoring touch throughout his career.",
+      imageURL:"haula.jpg"
+
   },
   {
     name: "Nico hischier",
-    number: "13",
+    number: 13,
     position: "forward",
-    age: "25",
+    age: 25,
     skills: ["precise playmaking', 'slick puck-handling', 'defensive responsibility', 'Skating"],
     biography:
       "Nico Hischier, hailing from Switzerland, quickly made his mark in the NHL as the first overall pick in the 2017 draft by the New Jersey Devils. With his dynamic skill set and leadership, he's become a cornerstone player for his team, embodying the future of Swiss hockey.",
+      imageURL:"nico.jpg"
+
   },
   {
     name: "Luke hughes",
-    number: "43",
+    number: 43,
     position: "Defensemen",
-    age: "20",
+    age: 20,
     skills: ["elite puck-moving skills', 'Stickhandling', 'offensive instincts', 'Skating"],
     biography:
       "Hailing from a hockey family with his brothers Jack and Quinn already making waves in the NHL, Luke Hughes made his mark in the USNTDP (United States National Team Development Program) with his smooth skating and offensive prowess from the blue line. With his combination of size, skill, and hockey IQ, he's poised to become a dynamic defenseman at the highest level of the game.",
+      imageURL:"luke.webp"
+
   },
   {
     name: "Curtis lazar",
-    number: "42 ",
+    number: 42,
     position: "Defensemen",
-    age: "29",
+    age: 29,
     skills: ["high speed', 'versatility', 'defensive reliability', 'faceoff proficiency"],
     biography:
       ". Hailing from Canada, Lazar has journeyed through various teams in the NHL, showcasing his skills as a dependable forward with a strong work ethic and a knack for making key plays when it matters most.",
+      imageURL:"cur.jpg"
+
   },
   {
     name: "Simon nemec",
-    number: "20",
+    number: 20,
     position: "Defensemen",
     age: "22",
     skills: ["high speed', 'Stickhandling', 'puck-handling abilities', 'Skating"],
     biography:
       "Nemec has risen through the ranks, showcasing his skills in various leagues and international competitions, carving out a reputation as a promising defenseman with potential to excel at the highest levels of the game  .",
+      imageURL:"sim.jpg"
+
   },
   {
     name: "Alexander holtz",
-    number: "10",
+    number: 10,
     position: "Defensemen",
-    age: "22",
+    age: 22,
     skills: ["smooth skating', 'scoring instincts', 'offensive creativity', 'ability to find open spaces on the ice"],
     biography:
       "Hailing from Saltsjö-Boo, Sweden, Holtz has made waves in junior leagues and international competitions, earning a reputation as a highly skilled sniper with the potential to become a prolific goal scorer at the professional level.",
+      imageURL:"alex.jpg"
+
   },
   {
     name: "Dawson mercer",
-    number: "91",
+    number: 91,
     position: "Defensemen",
-    age: "22",
+    age: 22,
     skills: ["high speed', 'hockey IQ', 'scoring touch', 'Skating"],
     biography:
       "Hailing from Newfoundland and Labrador, Canada, Mercer has risen through the junior ranks, impressing scouts and fans alike with his dynamic playmaking ability and determination to succeed at the highest levels of the game.",
+      imageURL:"dawson.jpg"
+
   },
   {
     name: "Jake allen",
-    number: "34",
+    number: 34,
     position: "Goalie",
-    age: "33",
+    age: 33,
     skills: ["quick reflexes', 'strong positional play', 'ability to make clutch saves under pressure', 'Skating"],
     biography:
       "Hailing from Fredericton, New Brunswick, Allen has established himself as a reliable netminder in the NHL, showcasing his skills with the St. Louis Blues and later with the Montreal Canadiens. With his consistent performance and calming presence in the crease, he's proven himself to be a valuable asset to any team.",
+      imageURL:"allen.jpg"
+
   },
   {
-    name: "Kaapo kakko",
-    number: "31",
+    name: " Nico Daws",
+    number: 50,
     position: "Goalie",
-    age: "23",
+    age: 23,
     skills: ["puck-handling skills', 'accurate shot', 'strong hockey IQ', 'Skating"],
     biography:
-      "Hailing from Turku, Finland, Kakko gained prominence in the Finnish Liiga before being selected.",
+      "Nicolas Daws is a German/Canadian professional ice hockey goaltender for the Utica Comets of the American Hockey League as a prospect to the New Jersey Devils of the National Hockey League. He was selected 84th overall by the Devils in the 2020 NHL Entry Draft.",
+      imageURL:"daws.jpg"
+
   },
 
 
@@ -125,8 +145,15 @@ function generateTeamCards() {
                   <div class = "card-header">
                       ${member.name}
                   </div>
+                  <img src="${member.imageURL}" class="image-fluid">
                   <div id="cardBody" class="card-body" style="background-color:${backgroundColor};">
                       <p><strong>Position:</strong> ${member.position}</p>
+                      <p><strong>Number:</strong> ${member.number}</p>
+                      <p><strong>age:</strong> ${member.age}</p>
+                      <p><strong>skills:</strong> ${member.skills}</p>
+                      <p><strong>biography:</strong> ${member.biography}</p>
+                      <p><strong>imageURL:</strong> ${member.imageURL}</p>
+                      
                   </div>
               </div>
           `;
